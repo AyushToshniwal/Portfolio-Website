08ede62edcab0b3f1b26c522e5e408fdc40a2e1e
YOUR_USERNAME <92023420+AyushToshniwal@users.noreply.github.com>
2024-06-13 21:40:49 +0530

    Initial commit

---
+++ src/app/data.js
@@ -0,0 +1,123 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+	{
+		id: 1,
+		name: "AI Text Summarizer",
+		description: "Summarize text using AI",
+		date: "",
+		demoLink: "https://text-summarizerrr.netlify.app/",
+	},
+	{
+		id: 2,
+		name: "Genius-AI",
+		description: "5 AI Tools At One Place",
+		date: "",
+		demoLink: "https://genius-ai-dun.vercel.app/",
+	},
+	{
+		id: 3,
+		name: "Modern Brainwave",
+		description: "Modern Design For AI Based Product",
+		date: "",
+		demoLink: "https://modernbrainwave.netlify.app/",
+	},
+	{
+		id: 4,
+		name: "Admin Dashboard",
+		description: "Admin Dashboard",
+		date: "",
+		demoLink: "https://ayushdashboard.netlify.app/",
+	},
+	{
+		id: 5,
+		name: "Gerichttt",
+		description: "Restaurant UI",
+		date: "",
+		demoLink: "https://gerichttt.netlify.app/",
+	},
+	// {
+	// 	id: 6,
+	// 	name: "JourneyLogger",
+	// 	description: "Log your travels",
+	// 	date: "2022-10-01",
+	// 	demoLink: "https://journeylogger.example.com",
+	// },
+	// {
+	// 	id: 7,
+	// 	name: "StudyBuddy",
+	// 	description: "Collaborative learning platform",
+	// 	date: "2022-04-18",
+	// 	demoLink: "https://studybuddy.example.com",
+	// },
+	// {
+	// 	id: 8,
+	// 	name: "TechTalk",
+	// 	description: "Tech news aggregator",
+	// 	date: "2022-11-05",
+	// 	demoLink: "https://techtalk.example.com",
+	// },
+	// {
+	// 	id: 9,
+	// 	name: "FitTrack",
+	// 	description: "Fitness and workout tracker",
+	// 	date: "2022-03-22",
+	// 	demoLink: "https://fittrack.example.com",
+	// },
+	// {
+	// 	id: 10,
+	// 	name: "MindfulMoments",
+	// 	description: "Meditation and mindfulness app",
+	// 	date: "2022-02-14",
+	// 	demoLink: "https://mindfulmoments.example.com",
+	// },
+];
+
+export const BtnList = [
+	{ label: "Home", link: "/", icon: "home", newTab: false },
+	{ label: "About", link: "/about", icon: "about", newTab: false },
+	{ label: "Projects", link: "/projects", icon: "projects", newTab: false },
+	{ label: "Contact", link: "/contact", icon: "contact", newTab: false },
+	{
+		label: "Github",
+		link: "https://github.com/AyushToshniwal",
+		icon: "github",
+		newTab: true,
+	},
+	{
+		label: "LinkedIn",
+		link: "https://www.linkedin.com/in/ayush-toshniwal-544989232/",
+		icon: "linkedin",
+		newTab: true,
+	},
+	{
+		label: "X",
+		link: "https://www.twitter.com",
+		icon: "twitter",
+		newTab: true,
+	},
+	{
+		label: "Resume",
+		link: "/resume.pdf",
+		icon: "resume",
+		newTab: true,
+	},
+];
+
+
